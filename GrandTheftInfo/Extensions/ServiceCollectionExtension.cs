7fb8e17232341f8edb84f7a88d8ce417de34eb7c
niko-tsonev <kambocha7@gmail.com>
2024-04-03 16:39:01 +0300

    Add project files.

---
+++ GrandTheftInfo/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrandTheftInfo.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+
+            return services;
+        }
+    }
+}
---
+++ GrandTheftInfo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GrandTheftInfo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
